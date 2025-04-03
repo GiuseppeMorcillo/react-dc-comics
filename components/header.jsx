028e49f2655058d6aec088d4046ddfa8c0c4da77
GiuseppeMorcillo <beppebeppe18@gmail.com>
2025-04-03 14:34:12 +0200

    feat:creato l'header del mio progetto

---
+++ components/header.jsx
@@ -0,0 +1,24 @@
+function Header() {
+    return <header>
+        <header className="header">
+            <div className="container">
+                <img src="/img/dc-logo.png" alt="DC Comics Logo" className="logo" />
+                <nav className="navMain">
+                    <ul className="nav-list">
+                        <li><a href="#">CHARACTERS</a></li>
+                        <li><a href="#">COMICS</a></li>
+                        <li><a href="#">MOVIES</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">GAMES</a></li>
+                        <li><a href="#">COLLECTIBLES</a></li>
+                        <li><a href="#">VIDEOS</a></li>
+                        <li><a href="#">FANS</a></li>
+                        <li><a href="#">NEWS</a></li>
+                        <li><a href="#">SHOP</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    </header>
+}
+export default Header;
