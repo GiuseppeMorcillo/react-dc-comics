58b7bd1510766ba60b9a48bc4a562e530a7bfd6e
GiuseppeMorcillo <beppebeppe18@gmail.com>
2025-04-03 15:06:22 +0200

    feat: creato un nuovo compontent per il main e creato il contentu html e css importando el immagine usate

---
+++ components/main.jsx
@@ -0,0 +1,19 @@
+function Main() {
+    return <main>
+        <div className="containerMain">
+            <div className="content-here">
+                <h2>Content Here</h2>
+            </div>
+            <div className="digital-comics">
+                <ul>
+                    <li><a href="#"><img src="/img/buy-comics-digital-comics.png" alt="buy comics digital" />Digital Comics</a></li>
+                    <li><a href="#"><img src="/img/buy-comics-merchandise.png" alt="buy comics digital" />DC Merchandise</a></li>
+                    <li><a href="#"><img src="/img/buy-comics-subscriptions.png" alt="buy comics digital" />Subscription</a></li>
+                    <li><a href="#"><img src="/img/buy-comics-shop-locator.png" alt="buy comics digital" />Comic Shop Locator</a></li>
+                    <li><a href="#"><img src="/img/buy-dc-power-visa.svg" alt="buy comics digital" />DC Power Visa</a></li>
+                </ul>
+            </div>
+        </div>
+    </main>
+}
+export default Main;
