16b554659497f5e77ed2e8c06e9084c98f180bb3
GiuseppeMorcillo <beppebeppe18@gmail.com>
2025-04-04 14:04:25 +0200

    feat: aggiunto i vari link tramine l'array

--- components/header.jsx
+++ components/header.jsx
@@ -1,20 +1,20 @@
-function Header() {
+function Header({ links }) {
     return <header>
         <header className="header">
             <div className="container">
                 <img src="/img/dc-logo.png" alt="DC Comics Logo" className="logo" />
                 <nav className="navMain">
                     <ul className="nav-list">
-                        <li><a href="#">CHARACTERS</a></li>
-                        <li><a href="#">COMICS</a></li>
-                        <li><a href="#">MOVIES</a></li>
-                        <li><a href="#">TV</a></li>
-                        <li><a href="#">GAMES</a></li>
-                        <li><a href="#">COLLECTIBLES</a></li>
-                        <li><a href="#">VIDEOS</a></li>
-                        <li><a href="#">FANS</a></li>
-                        <li><a href="#">NEWS</a></li>
-                        <li><a href="#">SHOP</a></li>
+                        <li><a href="#">{links[0]}</a></li>
+                        <li><a href="#">{links[1]}</a></li>
+                        <li><a href="#">{links[2]}</a></li>
+                        <li><a href="#">{links[3]}</a></li>
+                        <li><a href="#">{links[4]}</a></li>
+                        <li><a href="#">{links[5]}</a></li>
+                        <li><a href="#">{links[6]}</a></li>
+                        <li><a href="#">{links[7]}</a></li>
+                        <li><a href="#">{links[8]}</a></li>
+                        <li><a href="#">{links[9]}</a></li>
                     </ul>
                 </nav>
             </div>
