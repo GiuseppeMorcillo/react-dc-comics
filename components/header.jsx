2f1cef13b0caefc0c7e6442dd156baf67e2e0abc
GiuseppeMorcillo <beppebeppe18@gmail.com>
2025-04-04 15:27:39 +0200

    feat: prova

--- components/header.jsx
+++ components/header.jsx
@@ -5,16 +5,11 @@ function Header({ links }) {
                 <img src="/img/dc-logo.png" alt="DC Comics Logo" className="logo" />
                 <nav className="navMain">
                     <ul className="nav-list">
-                        <li><a href="#">{links[0]}</a></li>
-                        <li><a href="#">{links[1]}</a></li>
-                        <li><a href="#">{links[2]}</a></li>
-                        <li><a href="#">{links[3]}</a></li>
-                        <li><a href="#">{links[4]}</a></li>
-                        <li><a href="#">{links[5]}</a></li>
-                        <li><a href="#">{links[6]}</a></li>
-                        <li><a href="#">{links[7]}</a></li>
-                        <li><a href="#">{links[8]}</a></li>
-                        <li><a href="#">{links[9]}</a></li>
+                        {links.map((link, index) => (
+                            <li key={index}>
+                                <a href="#">{link}</a>
+                            </li>
+                        ))}
                     </ul>
                 </nav>
             </div>
